fafd827dd54cc3f5859914e488db0597d5467ea1
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-04-19 23:35:50 +0530

    Entering AI of Enemy
    Calculating Chase Conditions

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+        private void Update()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            float distance = Vector3.Distance(player.transform.position, transform.position);
+            if (distance < chaseDistance)
+            {
+                print(transform.name + " will chase");
+            }
+        }
+    }
+
+}
