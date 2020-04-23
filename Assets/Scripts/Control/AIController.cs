b179e4b4658c8793a991498fdc185d67398eb589
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-04-23 12:30:55 +0530

    Adding suspicion behaviour

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -10,13 +10,14 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5.0f;
-
+        [SerializeField] float suspicionTime = 3.0f;
         GameObject player;
         Fighter fighter;
         Health health;
         Mover mover;
 
         Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
         private void Start()
         {
             fighter = GetComponent<Fighter>();
@@ -32,13 +33,37 @@ namespace RPG.Control
             if (health.IsDead) return;
             if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
+                timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
+            }
+            else if (timeSinceLastSawPlayer < suspicionTime)//TODO:
+            {
+                //Suspicion state
+                SuspicionBehaviour();
+
             }
             else
             {
                 //fighter.Cancel(); // Starting a mover automatically cancels fighter
-                mover.StartMoveAction(guardPosition);
+                GuardBehaviour();
             }
+            timeSinceLastSawPlayer += Time.deltaTime;
+        }
+
+        private void GuardBehaviour()
+        {
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+            //  mover.StartMoveAction(player.transform.position); // Not in Idle state
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
         }
 
         private bool InAttackRangeOfPlayer()
