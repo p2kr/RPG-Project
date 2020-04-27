4055f83885d3e0995ac0215ba7561d40e29d6002
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-04-27 14:45:09 +0530

    Adding portal to another scene

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                print("Portal Triggered");
+                // Only specifying the sceneName or sceneBuildIndex 
+                //will load the Scene with the Single mode
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+}
